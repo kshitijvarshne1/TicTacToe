dc5276201438bc82eeb8dabb2d04430aaa181ee9
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-19 16:49:06 +0530

    add features of clicking of X in MainActivity.kt

--- app/src/main/java/varshney/kshitij/tictactoe/MainActivity.kt
+++ app/src/main/java/varshney/kshitij/tictactoe/MainActivity.kt
@@ -8,6 +8,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    var PLAYER = true
+    var TURN_COUNT = 0
+    var boardStatus = Array(3) { IntArray(3) }
     lateinit var board: Array<Array<Button>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,39 +27,67 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 button.setOnClickListener(this)
             }
         }
-        resetBtn.setOnClickListener { }
+
+        initializeBoardStatus()
+
+        resetBtn.setOnClickListener {
+            PLAYER = true
+            TURN_COUNT = 0
+            initializeBoardStatus()
+
+        }
+    }
+
+    private fun initializeBoardStatus() {
+        for (i in 0..2) {
+            for (j in 0..2) {
+                boardStatus[0][0] == -1
+                board[0][0].isEnabled = true
+                board[0][0].text = ""
+            }
+        }
     }
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.button -> {
-
+                updateValue(row = 0, col = 0, player = PLAYER)
             }
             R.id.button2 -> {
-
+                updateValue(row = 0, col = 1, player = PLAYER)
             }
             R.id.button3 -> {
-
+                updateValue(row = 0, col = 2, player = PLAYER)
             }
             R.id.button4 -> {
-
+                updateValue(row = 1, col = 0, player = PLAYER)
             }
             R.id.button5 -> {
-
+                updateValue(row = 1, col = 1, player = PLAYER)
             }
             R.id.button6 -> {
-
+                updateValue(row = 1, col = 2, player = PLAYER)
             }
             R.id.button7 -> {
-
+                updateValue(row = 2, col = 0, player = PLAYER)
             }
             R.id.button8 -> {
-
+                updateValue(row = 2, col = 1, player = PLAYER)
             }
             R.id.button9 -> {
-
+                updateValue(row = 2, col = 2, player = PLAYER)
             }
 
         }
+    }
+
+    private fun updateValue(row: Int, col: Int, player: Boolean) {
+        var text = if (player) "X" else "0"
+        var value = if (player) 1 else 0
+        board[row][col].apply {
+            isEnabled = false
+            setText(text)
+        }
+        boardStatus[row][col] = value
     }
 }
